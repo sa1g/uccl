a0916bfdb6382793eee9b05b19a067e00461d062
ettore <ettoreitaut4.1@gmail.com>
2026-05-08 10:30:17 +0200

    [uccl/rdma] nvc++ compiler support

--- collective/rdma/swift.h
+++ collective/rdma/swift.h
@@ -52,10 +52,20 @@ class SwiftCC {
   static constexpr double kFSRange = 5 * kBaseDelay;
   static constexpr double kFSMinCwnd = 32;   // in MTU-sized packets
   static constexpr double kFSMaxCwnd = 100;  // in MTU-sized packets
+  #ifndef __NVCOMPILER
   static constexpr double kFSAlpha = kFSRange / ((1.0 / std::sqrt(kFSMinCwnd)) -
                                                  (1.0 / std::sqrt(kFSMaxCwnd)));
   static constexpr double kFSBeta = -kFSAlpha / std::sqrt(kFSMaxCwnd);
+  #else
+  inline double getFSAlpha() const {
+    return kFSRange / ((1.0 / std::sqrt(kFSMinCwnd)) - 
+                        (1.0 / std::sqrt(kFSMaxCwnd)));
+  }
 
+  inline double getFSBeta() const {
+      return -getFSAlpha() / std::sqrt(kFSMaxCwnd);
+  }
+  #endif // __NVCOMPILER
   double rate_ = 0.0;  ///< The current sending rate
   size_t last_decrease_tsc_ = 0;
   double rtt_ = kBaseDelay;
@@ -126,7 +136,11 @@ class SwiftCC {
   }
 
   double get_target_delay() const {
+    #ifndef __NVCOMPILER
     double fs_delay = kFSAlpha / std::sqrt(prev_cwnd_ / kMSS) + kFSBeta;
+    #else
+    double fs_delay = getFSAlpha() / std::sqrt(prev_cwnd_ / kMSS) + getFSBeta();
+    #endif // __NVCOMPILER
 
     if (fs_delay > kFSRange) {
       fs_delay = kFSRange;
